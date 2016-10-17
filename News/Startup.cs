7553823c6c69efa3fceb68264b518da4090249b5
stu <stu@qq.com>
2016-10-17 16:20:36 +0800

    添加项目文件。

---
+++ News/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(News.Startup))]
+namespace News
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
