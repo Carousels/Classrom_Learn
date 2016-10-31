e7366b6baa020c978b711c2bd23e2e7769c4989d
stu <stu@qq.com>
2016-10-31 17:32:26 +0800

    添加博客和显示内容

---
+++ News/BlogDatabase.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace News
+{
+    public class BlogDatabase : DbContext
+    {
+        public BlogDatabase()
+            : base("name=BlogDatabase")
+        {
+        }
+
+
+        public virtual DbSet<Blog> Blogs { get; set; }
+        public virtual DbSet<BlogArticle> BlogArticles { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            var blogTable = modelBuilder.Entity<Blog>();
+            var blogArtticleTable = modelBuilder.Entity<BlogArticle>();
+
+            //blogTable.Property(o => o.Id).IsRequired();
+            blogTable.HasKey(o => o.Id);
+
+            //blogArtticleTable.Property(o => o.Id).IsRequired();
+            blogArtticleTable.HasKey(o => o.Id);
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+
+    /// <summary>
+    /// 博客数据
+    /// </summary>
+
+    public class Blog
+    {
+        /// <summary>
+        /// 博客ID
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 博客标题
+        /// </summary>
+        public string Title { get; set; }
+
+    }
+
+    /// <summary>
+    /// 博客文章
+    /// </summary>
+    public class BlogArticle
+    {
+        public int Id { get; set; }
+
+        public int BlogId { get; set; }
+
+        /// <summary>
+        /// 文章标题
+        /// </summary>
+        public string Subject { get; set; }
+
+        /// <summary>
+        /// 文章内容
+        /// </summary>
+
+        public string Body { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+
+        public DateTime DateCreated { get; set; }
+    }
+
+
+}
---
+++ News/Controllers/BlogController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace News.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            var db = new BlogDatabase();
+
+            db.Database.CreateIfNotExists();
+
+            var lst = db.BlogArticles.OrderByDescending(o => o.Id).ToList();
+            ViewBag.BlogArticles = lst;
+
+            return View();
+        }
+    }
+}
